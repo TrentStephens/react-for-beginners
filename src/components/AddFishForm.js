3921d8cb10c8de613cf1d07317259f2501e7704e
Trent Stephens <tstephens@TheNCC.edu>
2018-10-27 21:26:48 -0400

    Update 10.27.18

---
+++ src/components/AddFishForm.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+class AddFishForm extends React.Component {
+  nameRef = React.createRef();
+  priceRef = React.createRef();
+  statusRef = React.createRef();
+  descRef = React.createRef();
+  imageRef = React.createRef();
+
+
+  createFish = (event) => {
+    event.preventDefault();
+    const fish = {
+      name: this.nameRef.value.value,
+      price: parseFloat(this.priceRef.value.value),
+      status: this.statusRef.value.value,
+      desc: this.descRef.value.value,
+      imagine: this.imageRef.value.value,
+    }
+    this.props.addFish(fish);
+    event.currentTarget.reset();
+  };
+
+  render(){
+    return(
+      <form className="fish-edit" onSubmit={this.createFish}>
+        <input className ="name" ref={this.nameRef} type="text" placeholder="Name" />
+        <input className ="price" ref={this.priceRef} type="text" placeholder="Price" />
+        <select className  ="status" ref={this.statusRef}>
+          <option value="avaliable">Fresh!</option>
+          <option value="unavaliable">Sold Out!</option>
+        </select>
+        <textarea className ="desc" ref={this.descRef} placeholder="Desc" />
+        <input className ="image" ref={this.imageRef} type="text" placeholder="Image" />
+        <button type="submit">+ Add Fish</button>
+      </form>
+    );
+  }
+}
+
+export default AddFishForm;
